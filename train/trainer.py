91253446fbc7add9d674ee6299468156ac9e2f3a
Harish <harish.ramachandran@tum.de>
2025-04-15 08:33:27 +0200

    subclassed and overriden compute_loss function from Trainer

--- train/trainer.py
+++ train/trainer.py
@@ -10,16 +10,18 @@ class Trainer(Trainer_):
 
     def _model_forward(self, model, inputs):  ##custom function, not inside transformers library
         ##add the autoregressive part here
-        outputs = model(**inputs)
+        outputs,labels = model(**inputs)
 
-        return outputs
+        return outputs,labels
 
-    def compute_loss(self, model, inputs, return_outputs=False):   ##overrides the one in the  base class from transformers library
-        if self.label_smoother is not None and "labels" in inputs:
-            labels = inputs.pop("labels")
-        else:
-            labels = None
-        outputs = self._model_forward(model, inputs)
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):   ##overrides the one in the  base class from transformers library
+        # if self.label_smoother is not None and "labels" in inputs:
+        #     labels = inputs.pop("labels")
+        # else:
+        #     labels = None
+        #     #create a warning here if labels is None
+        #     assert labels is None, "Labels are None. Please check the inputs dictionary"
+        outputs,labels = self._model_forward(model, inputs)
                 
         #compute the loss here. Assume l2 loss
         loss_fn = nn.functional.mse_loss
