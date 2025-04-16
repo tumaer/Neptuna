8078d50f394587cee0095cbc3c7cbb648cbafa8c
Harish <harish.ramachandran@tum.de>
2025-04-16 07:36:22 +0200

    added data_config to the fetch_model_2

--- utils/load_model.py
+++ utils/load_model.py
@@ -1,18 +1,20 @@
 
 from typing import Dict
 
-def fetch_model(model_config: Dict):
+def fetch_model(model_config: Dict,
+                data_config: Dict):
     """
     Instantiate the model based on the provided model details.
     """
     model_name = model_config['model_name'].lower()
 
+
     if model_name == "fno":
         from models.fno.fno import FNO
         model = FNO(
                     dimension=model_config['dimension'],
-                    in_channels=model_config['in_channels'],#right now no historic steps will work
-                    out_channels=model_config['out_channels'], #currently only 1 step ahead prediction
+                    in_channels=model_config['in_channels']*data_config["sequence_info"][0][0],#right now no historic steps will work
+                    out_channels=model_config['out_channels']*data_config["sequence_info"][0][1], #currently only 1 step ahead prediction
                     latent_channels=model_config['latent_channels'],
                     num_fno_modes=eval(model_config['fno_modes']),
                     num_fno_layers=model_config['fno_layers'],
