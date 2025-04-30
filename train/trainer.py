63fd0fd116e31b0d8ffe0cc0e8a2c39d6a5771b3
Harish <harish.ramachandran@tum.de>
2025-04-30 08:17:00 +0200

    some basic cleanup

--- train/trainer.py
+++ train/trainer.py
@@ -15,16 +15,12 @@ class Trainer(Trainer_):
         return outputs,labels
 
     def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):   ##overrides the one in the  base class from transformers library
-        # if self.label_smoother is not None and "labels" in inputs:
-        #     labels = inputs.pop("labels")
-        # else:
-        #     labels = None
-        #     #create a warning here if labels is None
-        #     assert labels is None, "Labels are None. Please check the inputs dictionary"
+
         outputs,labels = self._model_forward(model, inputs)
                 
         #compute the loss here. Assume l2 loss
         loss_fn = nn.functional.mse_loss
-        loss = loss_fn(outputs, labels)
-
+        loss = loss_fn(outputs, labels) #the loss which is printed is rounded off to 4 deicmal places
+        #printing happening inside the function: _maybe_log_save_evaluate()
+        #print(f"loss o: {loss}")
         return (loss, outputs) if return_outputs else loss
