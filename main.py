b10e35d18e10713a0b708a6486640392290e136c
Harish <harish.ramachandran@tum.de>
2025-05-03 21:42:02 +0200

    added ResNet and made some small changes in the config files

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def main(config: DictConfig):
     start_time = time.time()
     trainer.train()
     end_time = time.time()
-    print(f"Total process time: {end_time - start_time:.2f} seconds")
+    print(f"Total train time: {end_time - start_time:.2f} seconds")
     
 if __name__=="__main__":
     main()
