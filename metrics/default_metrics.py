906f6f703fceb74a275da121c81acb7390cd61ec
Harish <harish.ramachandran@tum.de>
2025-06-11 11:17:22 +0200

    added separate metric for  l1 and l2

--- metrics/default_metrics.py
+++ metrics/default_metrics.py
@@ -1,9 +1,19 @@
 import numpy as np
+from transformers.trainer_utils import EvalPrediction
 
+def l1_error(preds, targets): #MAE loss
+    num_samples, num_eval_rollouts_plus_one, label_seq_length, num_channels, *spatial_dims = preds.shape
+    preds = preds.reshape(num_samples, num_eval_rollouts_plus_one*label_seq_length*num_channels, -1) #shape: (270,9,160)
+    targets = targets.reshape(num_samples, num_eval_rollouts_plus_one*label_seq_length*num_channels, -1) #shape: (270,9,160)
+    diff = (preds - targets)
+    l1_error = np.mean(np.abs(diff))
+    return l1_error
+    #270 is the number of accumulated samples == len(eval_dataset)
 
-def lp_error(preds: np.ndarray, targets: np.ndarray, p=1):
-    num_samples, num_channels, _, _ = preds.shape
-    preds = preds.reshape(num_samples, num_channels, -1)
-    targets = targets.reshape(num_samples, num_channels, -1)
-    errors = np.sum(np.abs(preds - targets) ** p, axis=-1)
-    return np.sum(errors, axis=-1) ** (1 / p)
+def l2_error(preds, targets): #RMSE loss
+    num_samples, num_eval_rollouts_plus_one, label_seq_length, num_channels, *spatial_dims = preds.shape
+    preds = preds.reshape(num_samples, num_eval_rollouts_plus_one*label_seq_length*num_channels, -1) #shape: (270,9,160)
+    targets = targets.reshape(num_samples, num_eval_rollouts_plus_one*label_seq_length*num_channels, -1) #shape: (270,9,160)
+    diff = (preds - targets)
+    l2_error = np.mean(np.abs(diff) ** 2) ** 0.5
+    return l2_error
