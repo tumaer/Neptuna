4179516cd412c5908c9c680dd1bb23c1925a2b5c
Jiadong Song <jiadong.song@tum.de>
2025-06-19 14:46:16 +0200

    improve the structure of resnet used in deeponet to reduce the memory consumption

--- models/DeepONet/deeponet_utils.py
+++ models/DeepONet/deeponet_utils.py
@@ -91,6 +91,68 @@ class CnnBranch(nn.Module):
                 assert out[i] > 0, f"Output shape is non-positive: {out[i]}"
         return tuple(out)
 
+class BasicBlockND4DeepONet(nn.Module):
+
+    expansion: int = 1
+
+    def __init__(
+        self,
+        in_planes: int,
+        planes: int,
+        dimension: int, 
+        stride: int = 1,
+        activation_fn: nn.Module = nn.GELU(),
+        norm: bool = True,
+        n_groups: int = 1,
+    ) -> None:
+        super().__init__()
+        
+        if dimension == 1:
+            Conv = nn.Conv1d
+            Pool = nn.MaxPool1d
+            kernel_size = (3,)
+            padding = (1,)
+        elif dimension == 2:
+            Conv = nn.Conv2d
+            Pool = nn.MaxPool2d
+            kernel_size = (3, 3)
+            padding = (1, 1)
+        elif dimension == 3:
+            Conv = nn.Conv3d
+            Pool = nn.MaxPool3d
+            kernel_size = (3, 3, 3)
+            padding = (1, 1, 1)
+        else:
+            raise ValueError(f"Unsupported dimension: {dimension}. Must be 1, 2, or 3.")
+        
+        #2X 3*3 convolutions Layers and corresponding GroupNorm
+        self.conv1 = Conv(in_planes, planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=True)
+        self.bn1 = nn.GroupNorm(n_groups, num_channels=planes) if norm else nn.Identity()
+        self.maxpool = Pool(2)
+        self.conv2 = Conv(planes, planes, kernel_size=kernel_size, stride=1, padding=padding, bias=True)
+        self.bn2 = nn.GroupNorm(n_groups, num_channels=planes)
+        self.activation = activation_fn
+
+        # Shortcut connection
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                Conv(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.GroupNorm(n_groups, self.expansion * planes) if norm else nn.Identity(),
+            )
+
+    def forward(self, x: Tensor) -> Tensor:
+        #out = self.conv1(self.activation(self.bn1(x)))
+        out = self.activation(self.bn1(self.conv1(x)))
+        #out = self.conv2(self.activation(self.bn2(out)))
+        out = self.activation(self.bn2(self.conv2(out)))
+        out = out + self.shortcut(x)
+        out = self.maxpool(out)
+        return out
+
+
+
+
 def grid_to_points(value: Tensor) -> Tuple[Tensor, List[int]]:
     """
     Convert from grid-based (1D, 2D, 3D) representation to point-based representation.
@@ -130,6 +192,7 @@ def points_to_grid(value: Tensor, shape: List[int]) -> Tensor:
 def calc_resnet_out_shape(
     in_shape: tuple,
     num_blocks: List[int],
+    if_maxpool: bool = False,
     stride: int=1,
     kernel_size: int=3,
     padding: int=1,
@@ -140,13 +203,17 @@ def calc_resnet_out_shape(
         count += num_blocks[j]
     for _ in range(count):
         for i in range(len(out)):
-            # 第一个conv
-            out[i] = (out[i] + 2 * padding - kernel_size) // stride + 1
-            # 第二个conv（stride=1, padding同上）
-            out[i] = (out[i] + 2 * padding - kernel_size) // 1 + 1
-            # shortcut不改变空间尺寸
-            assert out[i] > 0, f"Output shape is non-positive: {out[i]}"
+            if if_maxpool:
+                out[i] = (out[i] + 2*1 - 3) // stride + 1
+                out[i] = (out[i] + 2*1 - 3) // 1 + 1
+                out[i] = (out[i] - 2) // 2 + 1
+                assert out[i] > 0, f"Output shape is non-positive: {out[i]}"
+            else:
+                out[i] = (out[i] + 2 * padding - kernel_size) // stride + 1
+                out[i] = (out[i] + 2 * padding - kernel_size) // 1 + 1
+                assert out[i] > 0, f"Output shape is non-positive: {out[i]}"
     return tuple(out)
+
 def linspace_int_list(int1: int, int2: int, int3: int, reverse: bool) -> list:
     arr = [int(round(x)) for x in np.linspace(int3, int1, int2)]
     arr[0] = int3
