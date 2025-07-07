53f8bf23eeb9438ae4fad3abc0d0aabfbdc47232
Harish <harish.ramachandran@tum.de>
2025-07-07 13:08:22 +0200

    added code to visualize the tree structure of the .h5 file

--- misc/dataset_structure_viz.py
+++ misc/dataset_structure_viz.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import os
 
-data_path="./data/fluids/KS/1D/train.h5"
+data_path="./data/fluids/KVS_trimmed/2D/train.h5"
 data_path=os.path.abspath(data_path)
 print("data_path:",data_path)
 
@@ -11,5 +11,5 @@ with h5py.File(data_path, "r") as f:
         if isinstance(obj, h5py.Group):
             print(f"Group: {name}")
         elif isinstance(obj, h5py.Dataset):
-            print(f"  Dataset: {name} - Shape: {obj.shape}, Dtype: {obj.dtype}")
+            print(f"  Field: {name} - Shape: {obj.shape}, Dtype: {obj.dtype}")
     f.visititems(print_structure)
