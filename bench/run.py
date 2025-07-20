4001de1968e4ad84c2d5b28c18f243acc0c1f707
Harish <harish.ramachandran@tum.de>
2025-07-20 19:23:16 +0200

    set use_cpu keyword in train_config

--- bench/run.py
+++ bench/run.py
@@ -108,7 +108,7 @@ def run(cfg):
         auto_find_batch_size=False,
         full_determinism=False,  # turn on for reproducible distributed training
         torch_compile=False,
-        use_cpu=False,  # use_cpu even if other devices are present
+        use_cpu=cfg["train_config"]["use_cpu"],  # use_cpu even if other devices are present
         label_names=["label_including_rollouts"],
         disable_tqdm=True if cfg["output_log_config"]["logging"]["wandb"] else False,
         # ------------------------------------------------------------------
