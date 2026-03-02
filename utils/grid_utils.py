0a649a78a0d4b0d8b459870fcd034c28b97475f9
Harish <harish.ramachandran@tum.de>
2026-03-02 22:38:02 +0100

    updated grid_utils for scot3d

--- utils/grid_utils.py
+++ utils/grid_utils.py
@@ -48,6 +48,29 @@ def twod_meshgrid(shape: List[int], device: torch.device) -> Tensor:
     grid_y = grid_y.unsqueeze(0).unsqueeze(0).repeat(bsize, 1, 1, 1)
     return torch.cat((grid_x, grid_y), dim=1)
 
+def twod_meshgrid_3d(shape: List[int], device: torch.device) -> Tensor:
+    """Creates 2D meshgrid feature
+
+    Parameters
+    ----------
+    shape : List[int]
+        Tensor shape
+    device : torch.device
+        Device model is on
+
+    Returns
+    -------
+    Tensor
+        Meshgrid tensor
+    """
+    bsize, size_t, size_x, size_y = shape[0], shape[1], shape[3], shape[4]
+    grid_x = torch.linspace(0, 1, size_x, dtype=torch.float32, device=device)
+    grid_y = torch.linspace(0, 1, size_y, dtype=torch.float32, device=device)
+    grid_x, grid_y = torch.meshgrid(grid_x, grid_y, indexing="ij")
+    grid_x = grid_x.unsqueeze(0).unsqueeze(0).repeat(bsize, size_t, 1, 1, 1)
+    grid_y = grid_y.unsqueeze(0).unsqueeze(0).repeat(bsize, size_t, 1, 1, 1)
+    return torch.cat((grid_x, grid_y), dim=2)
+
 def threed_meshgrid(shape: List[int], device: torch.device) -> Tensor:
     """Creates 3D meshgrid feature
 
