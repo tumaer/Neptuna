5637688be9a2d5723a4c61998f3a070d8dcd4574
Harish <harish.ramachandran@tum.de>
2026-03-30 10:01:51 +0200

    cosmetic changes to poseidon, created utils

--- models/Poseidon/poseidon.py
+++ models/Poseidon/poseidon.py
@@ -1,7 +1,4 @@
-from transformers import (
-    Swinv2PreTrainedModel
-)
-from utils.model_utils import PretrainedConfig
+from transformers import Swinv2PreTrainedModel
 from transformers.models.swinv2.modeling_swinv2 import (
     Swinv2EncoderOutput,
     Swinv2Attention,
@@ -11,211 +8,19 @@ from transformers.models.swinv2.modeling_swinv2 import (
     window_reverse,
     window_partition,
 )
-from transformers.utils import ModelOutput
-from dataclasses import dataclass
+from models.Poseidon.poseidon_utils import (
+    ConditionalLayerNorm,
+    ConvNeXtBlock,
+    LayerNorm,
+    PoseidonConfig,
+    ResNetBlock,
+    ScOTOutput,
+)
 import torch
 from torch import nn
 from typing import Optional, Union, Tuple, List
 import math
 import collections
-
-
-@dataclass
-class ScOTOutput(ModelOutput):
-    loss: Optional[torch.FloatTensor] = None
-    output: torch.FloatTensor = None
-    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
-    attentions: Optional[Tuple[torch.FloatTensor]] = None
-    reshaped_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
-
-
-class PoseidonConfig(PretrainedConfig):
-    """https://github.com/huggingface/transformers/blob/v4.35.2/src/transformers/models/swinv2/configuration_swinv2.py"""
-
-    model_type = "swinv2"
-
-    attribute_map = {
-        "num_attention_heads": "num_heads",
-        "num_hidden_layers": "num_layers",
-    }
-
-    def __init__(
-        self,
-        image_size=224,
-        patch_size=4,
-        num_channels=3,
-        num_out_channels=1,
-        embed_dim=96,
-        depths=[2, 2, 6, 2],
-        num_heads=[3, 6, 12, 24],
-        skip_connections=[True, True, True],
-        window_size=7,
-        mlp_ratio=4.0,
-        qkv_bias=True,
-        hidden_dropout_prob=0.0,
-        attention_probs_dropout_prob=0.0,
-        drop_path_rate=0.1,
-        hidden_act="gelu",
-        use_absolute_embeddings=False,
-        initializer_range=0.02,
-        layer_norm_eps=1e-5,
-        p=1,  # for loss: 1 for l1, 2 for l2
-        channel_slice_list_normalized_loss=None,  # if None will fall back to absolute loss otherwise normalized loss with split channels
-        residual_model="convnext",  # "convnext" or "resnet"
-        use_conditioning=False,
-        learn_residual=False,  # learn the residual for time-dependent problems
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-
-        self.image_size = image_size
-        self.patch_size = patch_size
-        self.num_channels = num_channels
-        self.embed_dim = embed_dim
-        self.depths = depths
-        self.num_layers = len(depths)
-        self.num_heads = num_heads
-        self.skip_connections = skip_connections
-        self.window_size = window_size
-        self.mlp_ratio = mlp_ratio
-        self.qkv_bias = qkv_bias
-        self.hidden_dropout_prob = hidden_dropout_prob
-        self.attention_probs_dropout_prob = attention_probs_dropout_prob
-        self.drop_path_rate = drop_path_rate
-        self.hidden_act = hidden_act
-        self.use_absolute_embeddings = use_absolute_embeddings
-        self.use_conditioning = use_conditioning
-        self.learn_residual = learn_residual if self.use_conditioning else False
-        self.layer_norm_eps = layer_norm_eps
-        self.initializer_range = initializer_range
-        # we set the hidden_size attribute in order to make Swinv2 work with VisionEncoderDecoderModel
-        # this indicates the channel dimension after the last stage of the model
-        self.hidden_size = int(embed_dim * 2 ** (len(depths) - 1))
-        self.pretrained_window_sizes = (0, 0, 0, 0)
-        self.num_out_channels = num_out_channels
-        self.p = p
-        self.channel_slice_list_normalized_loss = channel_slice_list_normalized_loss
-        self.residual_model = residual_model
-
-
-class LayerNorm(nn.LayerNorm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def forward(self, x, time):
-        return super().forward(x)
-
-
-class ConditionalLayerNorm(nn.Module):
-    def __init__(self, dim, eps=1e-5):
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Linear(1, dim)
-        self.bias = nn.Linear(1, dim)
-
-    def forward(self, x, time):
-        mean = x.mean(dim=-1, keepdim=True)
-        var = (x**2).mean(dim=-1, keepdim=True) - mean**2
-        x = (x - mean) / (var + self.eps).sqrt()
-        time = time.reshape(-1, 1).type_as(x)
-        weight = self.weight(time).unsqueeze(1)
-        bias = self.bias(time).unsqueeze(1)
-        if x.dim() == 4:
-            weight = weight.unsqueeze(1)
-            bias = bias.unsqueeze(1)
-        return weight * x + bias
-
-
-class ConvNeXtBlock(nn.Module):
-    r"""Taken from: https://github.com/facebookresearch/ConvNeXt/blob/main/models/convnext.py
-    ConvNeXt Block. There are two equivalent implementations:
-    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
-    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
-    We use (2) as we find it slightly faster in PyTorch
-
-    Args:
-        dim (int): Number of input channels.
-        drop_path (float): Stochastic depth rate. Default: 0.0
-        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-    """
-
-    def __init__(self, config, dim, drop_path=0.0, layer_scale_init_value=1e-6):
-        super().__init__()
-        self.dwconv = nn.Conv2d(
-            dim, dim, kernel_size=7, padding=3, groups=dim
-        )  # depthwise conv
-        if config.use_conditioning:
-            layer_norm = ConditionalLayerNorm
-        else:
-            layer_norm = LayerNorm
-        self.norm = layer_norm(dim, eps=config.layer_norm_eps)
-        self.pwconv1 = nn.Linear(
-            dim, 4 * dim
-        )  # pointwise/1x1 convs, implemented with linear layers
-        self.act = nn.GELU()
-        self.pwconv2 = nn.Linear(4 * dim, dim)
-        self.weight = (
-            nn.Parameter(layer_scale_init_value * torch.ones((dim)), requires_grad=True)
-            if layer_scale_init_value > 0
-            else None
-        )  # was gamma before
-        self.drop_path = Swinv2DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
-
-    def forward(self, x, time, input_dimensions=None):
-        batch_size, sequence_length, hidden_size = x.shape
-        #! assumes square images
-        if input_dimensions is None:
-            input_height, input_width = math.floor(sequence_length**0.5)
-        else:
-            input_height, input_width = input_dimensions
-
-        input = x
-        x = x.reshape(batch_size, input_height, input_width, hidden_size)
-        x = x.permute(0, 3, 1, 2)
-        x = self.dwconv(x)
-        x = x.permute(0, 2, 3, 1)  # (N, C, H, W) -> (N, H, W, C)
-        x = self.norm(x, time)
-        x = self.pwconv1(x)
-        x = self.act(x)
-        x = self.pwconv2(x)
-        if self.weight is not None:
-            x = self.weight * x
-        x = x.reshape(batch_size, sequence_length, hidden_size)
-
-        x = input + self.drop_path(x)
-        return x
-
-
-class ResNetBlock(nn.Module):
-    def __init__(self, config, dim):
-        super().__init__()
-        kernel_size = 3
-        pad = (kernel_size - 1) // 2
-        self.conv1 = nn.Conv2d(dim, dim, kernel_size=kernel_size, stride=1, padding=pad)
-        self.conv2 = nn.Conv2d(dim, dim, kernel_size=kernel_size, stride=1, padding=pad)
-        self.bn1 = nn.BatchNorm2d(dim)
-        self.bn2 = nn.BatchNorm2d(dim)
-
-    def forward(self, x, time, input_shape=None):
-        batch_size, sequence_length, hidden_size = x.shape
-        #! assumes square images
-        if input_shape is None:
-            input_height, input_width = math.floor(sequence_length**0.5)
-        else:
-            input_height, input_width = input_shape
-
-        input = x
-        x = x.reshape(batch_size, input_height, input_width, hidden_size)
-        x = x.permute(0, 3, 1, 2)
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = nn.functional.leaky_relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = x.permute(0, 2, 3, 1)
-        x = x.reshape(batch_size, sequence_length, hidden_size)
-        x = x + input
-        return x
 
 
 class ScOTPatchEmbeddings(nn.Module):
@@ -227,8 +32,8 @@ class ScOTPatchEmbeddings(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        image_size, patch_size = config.image_size, config.patch_size
-        num_channels, hidden_size = config.num_channels, config.embed_dim
+        image_size, patch_size = config.grid_resolution, config.patch_size
+        num_channels, hidden_size = config.in_channels, config.latent_channels
         image_size = (
             image_size
             if isinstance(image_size, collections.abc.Iterable)
@@ -294,14 +99,14 @@ class ScOTEmbeddings(nn.Module):
         num_patches = self.patch_embeddings.num_patches
         self.patch_grid = self.patch_embeddings.grid_size
         self.mask_token = (
-            nn.Parameter(torch.zeros(1, 1, config.embed_dim))
+            nn.Parameter(torch.zeros(1, 1, config.latent_channels))
             if use_mask_token
             else None
         )
 
         if config.use_absolute_embeddings:
             self.position_embeddings = nn.Parameter(
-                torch.zeros(1, num_patches, config.embed_dim)
+                torch.zeros(1, num_patches, config.latent_channels)
             )
         else:
             self.position_embeddings = None
@@ -311,7 +116,7 @@ class ScOTEmbeddings(nn.Module):
         else:
             layer_norm = LayerNorm
 
-        self.norm = layer_norm(config.embed_dim)
+        self.norm = layer_norm(config.latent_channels)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(
@@ -370,11 +175,11 @@ class ScOTLayer(nn.Module):
             layer_norm = ConditionalLayerNorm
         else:
             layer_norm = LayerNorm
-        self.layernorm_before = layer_norm(dim, eps=config.layer_norm_eps)
+        self.layernorm_before = layer_norm(dim, eps=config.norm_layer_eps)
         self.drop_path = Swinv2DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
         self.intermediate = Swinv2Intermediate(config, dim)
         self.output = Swinv2Output(config, dim)
-        self.layernorm_after = layer_norm(dim, eps=config.layer_norm_eps)
+        self.layernorm_after = layer_norm(dim, eps=config.norm_layer_eps)
         
         # Cache for attention masks
         self.attn_mask_cache = {}
@@ -558,10 +363,10 @@ class ScOTPatchRecovery(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        image_size, patch_size = config.image_size, config.patch_size
+        image_size, patch_size = config.grid_resolution, config.patch_size
         num_out_channels, hidden_size = (
-            config.num_out_channels,
-            config.embed_dim,  # if not config.skip_connections[0] else 2 * config.embed_dim,
+            config.out_channels,
+            config.latent_channels,  # if not config.skip_connections[0] else 2 * config.latent_channels,
         )
         image_size = (
             image_size
@@ -957,7 +762,7 @@ class ScOTEncoder(nn.Module):
             [
                 ScOTEncodeStage(
                     config=config,
-                    dim=int(config.embed_dim * 2**i_layer),
+                    dim=int(config.latent_channels * 2**i_layer),
                     input_resolution=(
                         grid_size[0] // (2**i_layer),
                         grid_size[1] // (2**i_layer),
@@ -1094,7 +899,7 @@ class ScOTDecoder(nn.Module):
             [
                 ScOTDecodeStage(
                     config=config,
-                    dim=int(config.embed_dim * 2**i_layer),
+                    dim=int(config.latent_channels * 2**i_layer),
                     input_resolution=(
                         grid_size[0] // (2**i_layer),
                         grid_size[1] // (2**i_layer),
@@ -1228,7 +1033,7 @@ class Poseidon(Swinv2PreTrainedModel):
         self.config = config
         self.num_layers_encoder = len(config.depths)
         self.num_layers_decoder = len(config.depths)
-        self.num_features = int(config.embed_dim * 2 ** (self.num_layers_encoder - 1))
+        self.num_features = int(config.latent_channels * 2 ** (self.num_layers_encoder - 1))
 
         self.embeddings = ScOTEmbeddings(config, use_mask_token=use_mask_token)
         self.grid = self.embeddings.patch_grid
@@ -1248,7 +1053,7 @@ class Poseidon(Swinv2PreTrainedModel):
                 (
                     nn.ModuleList(
                         [
-                            res_model(config, config.embed_dim * 2**i)
+                            res_model(config, config.latent_channels * 2**i)
                             for _ in range(depth)
                         ]
                     )
@@ -1313,7 +1118,7 @@ class Poseidon(Swinv2PreTrainedModel):
 
         #! Add time if not provided
         if time is None:
-            time = torch.Tensor([0]).to(input_data.device)
+            time = torch.Tensor([0.05]).to(input_data.device)
 
         output_attentions = (
             output_attentions
@@ -1397,8 +1202,8 @@ class Poseidon(Swinv2PreTrainedModel):
         prediction = self.patch_recovery(sequence_output)
         # The following can be used for learning just the residual for time-dependent problems
         if self.config.learn_residual:
-            if self.config.num_channels > self.config.num_out_channels:
-                input_data = input_data[:, 0 : self.config.num_out_channels]
+            if self.config.in_channels > self.config.out_channels:
+                input_data = input_data[:, 0 : self.config.out_channels]
             prediction += input_data
 
         # if image_size != self.config.image_size:
